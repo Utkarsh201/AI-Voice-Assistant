f7eb837e96e6cee760ed8889f5ea1d35a5f5226b
Utkarsh Gupta <utkarsh3333gupta@gmail.com>
2025-10-29 18:14:19 +0530

    added the Dialogflow api

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import dotenv from "dotenv"
+import { SessionsClient } from '@google-cloud/dialogflow';
+import { v4 as uuid } from 'uuid';
 
 const app = express();
 
+dotenv.config();
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const keyPath = path.join(__dirname, 'config', 'config.json');
+
+process.env.GOOGLE_APPLICATION_CREDENTIALS = keyPath;
 
 app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
 
